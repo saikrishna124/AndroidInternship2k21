fab8b87c68eef5fc2d9b5b2e16410588dcf15799
saikrishna124 <saikrishnakukkala@gmail.com>
2021-05-31 23:20:50 +0530

    first commit

---
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+    Button toast,count;
+    TextView tv;
+    int i;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toast=findViewById(R.id.toast);
+        count=findViewById(R.id.count);
+        tv=findViewById(R.id.tv);
+        count.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i++;
+                tv.setText(""+i);
+            }
+        });
+        toast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Welcome to android your count is"+i,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
